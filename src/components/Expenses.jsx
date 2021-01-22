84fccffeff835676f94a0c2302af381877ead133
RoryMusinskas <devrorym@gmail.com>
2021-01-22 14:38:31 +1100

    Refactored file and removed unneeded imports. Left a large commented out section for reference as they were refactored

--- src/components/Expenses.jsx
+++ src/components/Expenses.jsx
@@ -1,11 +1,51 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
 export function Expenses() {
-  const { user, isAuthenticated, getAccessTokenSilently } = useAuth0();
-  const [userAccessToken, setUserAccessToken] = useState(null);
+  const { getAccessTokenSilently } = useAuth0();
   const [expenses, setExpenses] = useState([]);
 
+  // A cleaner way to fetch with token
+  const getData = async () => {
+    try {
+      const token = await getAccessTokenSilently();
+      const response = await fetch("http://localhost:3001/expenses", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      const responseData = await response.json();
+      setExpenses(responseData);
+    } catch (e) {
+      console.error("Error: ", e.message);
+    }
+  };
+
+  // A way to post expenses to the backend
+  const postExpense = async () => {
+    try {
+      const token = await getAccessTokenSilently();
+      await fetch("http://localhost:3001/expenses", {
+        method: "POST",
+        mode: "cors",
+        headers: {
+          "Content-type": "application/json",
+          Authorization: `bearer ${token}`,
+        },
+        body: JSON.stringify({
+          expense: {
+            description: "Placeholder",
+            amount: 100,
+            category_id: 1,
+          },
+        }),
+      });
+    } catch (e) {
+      console.error(e.message);
+    }
+  };
+  
+  // FUNCTIONS I HAVE REFACTORED - LEFT FOR REFERENCE 
   // A way to get access token not very clean
   // useEffect(() => {
   //   const getUserAccessToken = async () => {
@@ -21,51 +61,9 @@ export function Expenses() {
   //   getUserAccessToken();
   // }, [getAccessTokenSilently]);
 
-  // A cleaner way to fetch with token
-    const getData = async () => {
-      try {
-        const token = await getAccessTokenSilently()
-        const response = await fetch("http://localhost:3001/expenses",{
-          headers: {
-            Authorization: `Bearer ${token}`
-          },
-        })
-        const responseData = await response.json()
-        setExpenses(responseData);
-      } catch (e) {
-        console.error("Error: ", e.message);
-      }
-    };
-
   // useEffect(() => {
   //   getData();
   // }, [userAccessToken]);
-
-    const postExpense = async () => {
-      try{
-        const token = await getAccessTokenSilently()
-        await fetch("http://localhost:3001/expenses", {
-          method: "POST",
-          mode: "cors",
-          headers: {
-            "Content-type": "application/json",
-            Authorization: `bearer ${token}`,
-          },
-          body: JSON.stringify({
-            expense: {
-              description: "Placeholder",
-              amount: 100,
-              category_id: 1,
-            },
-            payload:{
-              email: user.email,
-            }
-          }),
-        });
-      }catch(e){
-        console.error(e.message)
-      }
-  };
 
   // const getData = () => {
   //   isAuthenticated &&
@@ -80,14 +78,15 @@ export function Expenses() {
 
   return (
     <>
-    <h1>Expenses</h1>
-    <button onClick={getData}>get data</button>
-    <button onClick={postExpense}>Post random expense</button>
-    <ul>
-      {expenses.map((expense) => {
-      return <li>{expense.description}</li>})}
-    </ul>
+      <h1>Expenses</h1>
+      <button onClick={getData}>get data</button>
+      <button onClick={postExpense}>Post random expense</button>
+      <ul>
+        {expenses.map((expense) => {
+          return <li>{expense.description}</li>;
+        })}
+      </ul>
     </>
   );
-// }
-  };
+  // }
+};
