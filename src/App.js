292783e60343933e8c419f59298c78eafacac523
RoryMusinskas <devrorym@gmail.com>
2021-01-22 11:52:53 +1100

    Currently WIP, passing in data to rails and getting current user back.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,61 @@
 import './App.css';
+import { useAuth0 } from "@auth0/auth0-react";
+import AuthenticationNav from './components/AuthenticationNav';
+import UserProfile from './components/UserProfile'
+import { Expenses } from './components/Expenses'
+import Loading from './components/Loading'
+import {Route, Switch} from 'react-router-dom'
+import ProtectedRoute from './auth/ProtectedRoute'
 
 function App() {
+  const { isAuthenticated, isLoading, getAccessTokenSilently, user } = useAuth0();
+
+  if (isLoading) {
+    return <Loading />
+  }
+  
+  if (isAuthenticated) {
+    const getUserToken = async () => {
+      try {
+        const UserAccessToken = await getAccessTokenSilently({
+          audience: `https://${process.env.REACT_APP_AUTH0_DOMAIN}/api/v2/`,
+          scope: "read:current_user",
+        });
+
+        const token = await getAccessTokenSilently();
+        fetch("http://localhost:3001/users", {
+          method: "POST",
+          mode: "cors",
+          headers: {
+            "Content-type": "application/json",
+            Authorization: `bearer ${token}`,
+          },
+          body: JSON.stringify({
+              user: {
+                email: user.email,
+                shares_preferences: ['test', 'test']
+              },
+              UserAccessToken: UserAccessToken,
+          }),
+        });
+      } catch (e) {
+        console.log(e.message);
+      }
+    };
+    getUserToken()
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Budget Tracker App</h1>
-        <hr />
-      </header>
+      <h1>Budget Tracker App</h1>
+      <hr />
+
+      <AuthenticationNav />
+      <Switch>
+        <Route exact path="/" />
+        <ProtectedRoute exact path="/profile" component={UserProfile} />
+        <ProtectedRoute exact pach="/expenses" component={Expenses} />
+      </Switch>
     </div>
   );
 }
