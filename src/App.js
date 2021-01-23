74d4505dd1b4af6561159a73bb9016f3c79d7924
RoryMusinskas <devrorym@gmail.com>
2021-01-23 17:02:10 +1100

    Added back in the fetch user data on load, this somehow got deleted and then was returning 422 request for auth0

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from "react";
-import { Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import ProtectedRoute from "./auth/ProtectedRoute";
 import { Profile } from "./components/UserProfile";
 import { Expenses } from "./components/Expenses";
+import LandingPage from "views/LandingPage/LandingPage";
+import { useAuth0 } from "@auth0/auth0-react";
+import Loading from "./components/Loading";
 
 // core components
 import Admin from "layouts/Admin";
@@ -11,14 +14,55 @@ import RTL from "layouts/RTL";
 import "assets/css/material-dashboard-react.css?v=1.9.0";
 
 export default function App() {
+  const {
+    isAuthenticated,
+    isLoading,
+    getAccessTokenSilently,
+    user,
+  } = useAuth0();
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  if (isAuthenticated === true) {
+    const getUserToken = async () => {
+      try {
+        const UserAccessToken = await getAccessTokenSilently({
+          audience: `https://${process.env.REACT_APP_AUTH0_DOMAIN}/api/v2/`,
+          scope: "read:current_user",
+        });
+
+        const token = await getAccessTokenSilently();
+        fetch("http://localhost:3001/users", {
+          method: "POST",
+          mode: "cors",
+          headers: {
+            "Content-type": "application/json",
+            Authorization: `bearer ${token}`,
+          },
+          body: JSON.stringify({
+            user: {
+              email: user.email,
+              shares_preferences: ["test", "test"],
+            },
+            UserAccessToken: UserAccessToken,
+          }),
+        });
+      } catch (e) {
+        console.log(e.message);
+      }
+    };
+    getUserToken();
+  }
   return (
-    <div>
+    <>
       <Switch>
+        <Route exact path="/" component={LandingPage} />
         <ProtectedRoute path="/admin" component={Admin} />
         <ProtectedRoute path="/rtl" component={RTL} />
         <ProtectedRoute path="/profile" component={Profile} />
         <ProtectedRoute path="/expenses" component={Expenses} />
       </Switch>
-    </div>
+    </>
   );
 }
