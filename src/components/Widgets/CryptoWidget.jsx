fc9e152d7e9c74f4df6ec21d50f873024c65c3ff
RoryMusinskas <devrorym@gmail.com>
2021-01-24 13:38:12 +1100

    Added in a basic crypto chart, not sure if we will use yet.

---
+++ src/components/Widgets/CryptoWidget.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect } from "react";
+
+export default function StockMarket() {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src =
+      "https://s3.tradingview.com/external-embedding/embed-widget-screener.js";
+    script.async = true;
+
+    script.innerHTML = JSON.stringify({
+      colorTheme: "light",
+      dateRange: "12M",
+      exchange: "ASX",
+      showChart: true,
+      locale: "en",
+      width: "100%",
+      height: "100%",
+      largeChartUrl: "",
+      isTransparent: false,
+      showSymbolLogo: false,
+      plotLineColorGrowing: "rgba(33, 150, 243, 1)",
+      plotLineColorFalling: "rgba(33, 150, 243, 1)",
+      gridLineColor: "rgba(152, 152, 152, 1)",
+      scaleFontColor: "rgba(120, 123, 134, 1)",
+      belowLineFillColorGrowing: "rgba(33, 150, 243, 0.12)",
+      belowLineFillColorFalling: "rgba(33, 150, 243, 0.12)",
+      symbolActiveColor: "rgba(33, 150, 243, 0.12)",
+    });
+    document.getElementById("crypto-currency-chart").appendChild(script);
+  }, []);
+
+  return <></>;
+}
