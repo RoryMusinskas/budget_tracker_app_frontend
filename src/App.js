5ed87c1639e1a165ab75dee02a584290f7a47f26
RoryMusinskas <devrorym@gmail.com>
2021-01-24 18:56:40 +1100

    Added in a nested try catch block, to prompt all users on first login to consent to the app. This is not ideal as it is a second popup, but it solves the no consent issue for now.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Route, Switch } from "react-router-dom";
 import ProtectedRoute from "./auth/ProtectedRoute";
 import { Profile } from "./components/UserProfile";
@@ -17,39 +17,65 @@ export default function App() {
   const {
     isAuthenticated,
     isLoading,
+    getAccessTokenWithPopup,
     getAccessTokenSilently,
     user,
+    error,
   } = useAuth0();
+
+  const [userAccessToken, setUserAccessToken] = useState("");
+
   if (isLoading) {
     return <Loading />;
   }
 
+  if (error) {
+    console.log("Error", error.message);
+  }
+
   if (isAuthenticated === true) {
+    // This async function will retrieve a userToken from the auth0 usermanagement endpoint
     const getUserToken = async () => {
+      // try to get this token silently with no popup
       try {
         const UserAccessToken = await getAccessTokenSilently({
           audience: `https://${process.env.REACT_APP_AUTH0_DOMAIN}/api/v2/`,
           scope: "read:current_user",
         });
-
-        const token = await getAccessTokenSilently();
-        fetch("http://localhost:3001/users", {
-          method: "POST",
-          mode: "cors",
-          headers: {
-            "Content-type": "application/json",
-            Authorization: `bearer ${token}`,
-          },
-          body: JSON.stringify({
-            user: {
-              email: user.email,
-              shares_preferences: ["test", "test"],
-            },
-            UserAccessToken: UserAccessToken,
-          }),
-        });
+        setUserAccessToken(UserAccessToken);
+        // if it can't get it silently, it will prompt the user to enable the consent (this will only happen on first login)
       } catch (e) {
-        console.log(e.message);
+        console.log(e);
+        const UserAccessToken = await getAccessTokenWithPopup({
+          audience: `https://${process.env.REACT_APP_AUTH0_DOMAIN}/api/v2/`,
+          scope: "read:current_user",
+        });
+        setUserAccessToken(UserAccessToken);
+      }
+
+      // If there is a user token, run the next try catch.
+      // This is needed the function to get the token above is async, this allows it to store the token first
+      if (userAccessToken !== "") {
+        try {
+          const token = await getAccessTokenSilently();
+          fetch("http://localhost:3001/users", {
+            method: "POST",
+            mode: "cors",
+            headers: {
+              "Content-type": "application/json",
+              Authorization: `bearer ${token}`,
+            },
+            body: JSON.stringify({
+              user: {
+                email: user.email,
+                shares_preferences: ["test", "test"],
+              },
+              UserAccessToken: userAccessToken,
+            }),
+          });
+        } catch (e) {
+          console.log(e.message);
+        }
       }
     };
     getUserToken();
