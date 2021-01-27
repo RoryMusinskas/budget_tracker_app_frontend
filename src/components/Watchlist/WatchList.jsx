2c2e3e6cd9a4eb0158f27634558ad9fc7e0944c0
RoryMusinskas <devrorym@gmail.com>
2021-01-27 22:45:38 +1100

    Moved logic from other components into here as it makes more sense and more modular

--- src/components/Watchlist/WatchList.jsx
+++ src/components/Watchlist/WatchList.jsx
@@ -1,9 +1,14 @@
+/* -------- Import React core ------------ */
 import React, { useEffect } from "react";
-import WatchlistModal from "components/Modal/WatchlistModal";
 import { useAuth0 } from "@auth0/auth0-react";
+/* -------- Import MaterialUI core ------------ */
+import WatchlistModal from "./WatchlistModal";
+/* -------- Import Custom Components ---------- */
 
 export default function WatchList(props) {
   const { getAccessTokenSilently } = useAuth0();
+
+  // get the users watchlist from the database on component load
   useEffect(() => {
     const getWatchList = async () => {
       try {
@@ -17,6 +22,7 @@ export default function WatchList(props) {
           }
         );
         const responseData = await response.json();
+        // set the state of the watchlist in the shares page, to then pass to the trading view widget
         props.setWatchList(responseData.shares_preferences);
       } catch (e) {
         console.error("Error: ", e.message);
@@ -24,5 +30,34 @@ export default function WatchList(props) {
     };
     getWatchList();
   }, []);
-  return <WatchlistModal watchList={props.watchList} />;
+
+  // update the watchlist array for the current user in the database
+  async function updateUserWatchList(data) {
+    try {
+      const token = await getAccessTokenSilently();
+      fetch(`${process.env.REACT_APP_RAILS_API_URL}/users/:id`, {
+        method: "PUT",
+        mode: "cors",
+        headers: {
+          "Content-type": "application/json",
+          Authorization: `bearer ${token}`,
+        },
+        body: JSON.stringify({
+          user: {
+            shares_preferences: data,
+          },
+        }),
+      });
+    } catch (e) {
+      console.log(e.message);
+    }
+  }
+
+  return (
+    <WatchlistModal
+      watchList={props.watchList}
+      setWatchList={props.setWatchList}
+      updateUserWatchList={updateUserWatchList}
+    />
+  );
 }
