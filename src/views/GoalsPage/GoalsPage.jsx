2d5aa6abdabde8da23d65acdb5227f90a616f226
RoryMusinskas <devrorym@gmail.com>
2021-01-30 15:23:46 +1100

    Added logic to post the initial data to the database, this now has the most basic of persistant data storage. This is a huge component which will need to be split up into smaller components. Will also need to add the ability for people to dynamically add/remove columns and goals.

--- src/views/GoalsPage/GoalsPage.jsx
+++ src/views/GoalsPage/GoalsPage.jsx
@@ -1,22 +1,25 @@
 /* -------- Import React core ------------ */
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef, useLayoutEffect } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
 /* -------- Import MaterialUI core ------------ */
 import Grid from "@material-ui/core/Grid";
 import { makeStyles } from "@material-ui/core/styles";
 /* -------- Import Custom Components ---------- */
 import Column from "components/Goals/Column";
 import { DragDropContext, Droppable } from "react-beautiful-dnd";
-
-import initialData from "components/Goals/initial-data";
+import * as initialData from "components/Goals/initial-data.json";
+import Loading from "components/Loading";
 
 import styles from "assets/jss/material-dashboard-react/views/goalsPageStyle";
 const useStyles = makeStyles(styles);
 
 export default function GoalsPage() {
   const classes = useStyles();
-  const [state, setState] = useState(initialData);
+  const { getAccessTokenSilently, user } = useAuth0();
+  const [state, setState] = useState(initialData.default);
+  const [loading, setLoading] = useState(true);
 
-  // this function is used to persist the order of the column
+  // this function is used to persist the order of the column, it's fired on drag end of a draggable component into a droppable component
   const onDragEnd = (result) => {
     // destructure info from the result of the drag end
     const { destination, source, draggableId, type } = result;
@@ -35,7 +38,8 @@ export default function GoalsPage() {
       return;
     }
 
-    // logic for reordering a column
+    //----------------- logic for reordering a column -----------------------//
+    //-----------------------------------------------------------------------//
     if (type === "column") {
       // create a new column order array with the values from before
       const newColumnOrder = Array.from(state.columnOrder);
@@ -50,9 +54,12 @@ export default function GoalsPage() {
         columnOrder: newColumnOrder,
       };
       setState(newState);
+      // console.log("column", newState);
       return;
     }
 
+    //--------- logic for reordering a goal within its start column ---------//
+    //-----------------------------------------------------------------------//
     // get the column from the state
     const start = state.columns[source.droppableId];
     const finish = state.columns[destination.droppableId];
@@ -83,12 +90,12 @@ export default function GoalsPage() {
 
       // update the state
       setState(newState);
-      return;
 
-      //TODO -- THIS SHOULD NOW CALL THE GOALS ENDPOINT AND UPDATE THE DATA
+      return;
     }
 
-    // Moving from one list to another
+    //-------------- logic for moving a goal to another column --------------//
+    //-----------------------------------------------------------------------//
 
     // create a new start goals array, with the ids from the old array
     const startGoalIds = Array.from(start.goalIds);
@@ -124,38 +131,138 @@ export default function GoalsPage() {
     };
 
     setState(newState);
+    // console.log("moving goal to anouther column", newState);
     return;
-
-    //TODO -- THIS SHOULD NOW CALL THE GOALS ENDPOINT AND UPDATE THE DATA
   };
 
-  return (
-    <DragDropContext onDragEnd={onDragEnd}>
-      <Droppable droppableId="all-columns" direction="horizontal" type="column">
-        {(provided) => (
-          <Grid
-            container
-            className={classes.goalsContainer}
-            {...provided.droppableProps}
-            ref={provided.innerRef}
-          >
-            {state.columnOrder.map((columnId, index) => {
-              const column = state.columns[columnId];
-              const goals = column.goalIds.map((goalId) => state.goals[goalId]);
+  //------ logic for persting data to the database for each user ----------//
+  //-----------------------------------------------------------------------//
+  useEffect(() => {
+    getUserGoalData();
+  }, []);
 
-              return (
-                <Column
-                  key={column.id}
-                  column={column}
-                  goals={goals}
-                  index={index}
-                />
-              );
-            })}
-            {provided.placeholder}
-          </Grid>
-        )}
-      </Droppable>
-    </DragDropContext>
-  );
+  // this is the equivilent of componentDidUpdate, this is so it doesn't run a put request on the initial load, as there is nothing in the database yet.
+  // this will update the values on each subsequent state update
+  const firstUpdate = useRef(true);
+  useLayoutEffect(() => {
+    if (firstUpdate.current) {
+      firstUpdate.current = false;
+      return;
+    }
+    putUserGoalData(state);
+  }, [state]);
+
+  // get the user data from the database, if they haven't got any data stored, post the default values, else update the state
+  const getUserGoalData = async () => {
+    try {
+      const token = await getAccessTokenSilently();
+      const response = await fetch(
+        `${process.env.REACT_APP_RAILS_API_URL}/goals`,
+        {
+          headers: {
+            Authorization: `bearer ${token}`,
+          },
+        }
+      );
+      const responseData = await response.json();
+      if (responseData.length === 0) {
+        postUserGoalData(state);
+      } else {
+        setState(responseData[0].goals_data);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+    setLoading(false);
+  };
+
+  // this is to post the intial data to the database on only the first time, when the user has no data stored in the database
+  const postUserGoalData = async () => {
+    try {
+      const token = await getAccessTokenSilently();
+      await fetch(`${process.env.REACT_APP_RAILS_API_URL}/goals`, {
+        method: "POST",
+        mode: "cors",
+        headers: {
+          "Content-type": "application/json",
+          Authorization: `bearer ${token}`,
+        },
+        body: JSON.stringify({
+          goals_data: {
+            goals: state.goals,
+            columns: state.columns,
+            columnOrder: state.columnOrder,
+          },
+          user_sub: user.sub,
+        }),
+      });
+    } catch (e) {
+      console.error(e.message);
+    }
+  };
+
+  // update the users data in the database, this will run for every state update, except the first one when there is no data in the database for the user
+  const putUserGoalData = async () => {
+    try {
+      const token = await getAccessTokenSilently();
+      await fetch(`${process.env.REACT_APP_RAILS_API_URL}/goals/${user.sub}`, {
+        method: "PUT",
+        mode: "cors",
+        headers: {
+          "Content-type": "application/json",
+          Authorization: `bearer ${token}`,
+        },
+        body: JSON.stringify({
+          goals_data: {
+            goals: state.goals,
+            columns: state.columns,
+            columnOrder: state.columnOrder,
+          },
+          user_sub: user.sub,
+        }),
+      });
+    } catch (e) {
+      console.error(e.message);
+    }
+  };
+
+  if (!loading) {
+    return (
+      <DragDropContext onDragEnd={onDragEnd}>
+        <Droppable
+          droppableId="all-columns"
+          direction="horizontal"
+          type="column"
+        >
+          {(provided) => (
+            <Grid
+              container
+              className={classes.goalsContainer}
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+            >
+              {state.columnOrder.map((columnId, index) => {
+                const column = state.columns[columnId];
+                const goals = column.goalIds.map(
+                  (goalId) => state.goals[goalId]
+                );
+
+                return (
+                  <Column
+                    key={column.id}
+                    column={column}
+                    goals={goals}
+                    index={index}
+                  />
+                );
+              })}
+              {provided.placeholder}
+            </Grid>
+          )}
+        </Droppable>
+      </DragDropContext>
+    );
+  } else {
+    return <Loading />;
+  }
 }
