bf08b43f3177ebcbc3271566ebd5fad885d29e4d
derricklai33 <derricklai33@hotmail.com>
2021-01-31 15:57:11 +1100

    Reorganised fetch request into useEffect

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -2,51 +2,65 @@ import React, { useEffect, useState } from "react";
 
 import { useAuth0 } from "@auth0/auth0-react";
 
-const useStyles = makeStyles(styles);
+// const useStyles = makeStyles(styles);
 
 export function Expenses() {
   const { getAccessTokenSilently } = useAuth0();
   const [expenses, setExpenses] = useState([]);
 
   // A cleaner way to fetch with token
-  async function fetchExpenses() {
-    try {
-      const token = await getAccessTokenSilently();
-      const response = await fetch(
-        `${process.env.REACT_APP_RAILS_API_URL}/expenses`,
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      );
-      const responseData = await response.json();
-      setExpenses(responseData);
-    } catch (e) {
-      console.error("Error: ", e.message);
-    }
-  }
+  // async function fetchExpenses() {
+  //   try {
+  //     const token = await getAccessTokenSilently();
+  //     const response = await fetch(
+  //       `${process.env.REACT_APP_RAILS_API_URL}/expenses`,
+  //       {
+  //         headers: {
+  //           Authorization: `Bearer ${token}`,
+  //         },
+  //       }
+  //     );
+  //     const responseData = await response.json();
+  //     setExpenses(responseData);
+  //   } catch (e) {
+  //     console.error("Error: ", e.message);
+  //   }
+  // }
 
   useEffect(() => {
+    async function fetchExpenses() {
+      try {
+        const token = await getAccessTokenSilently();
+        const response = await fetch(
+          `${process.env.REACT_APP_RAILS_API_URL}/expenses`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        const responseData = await response.json();
+        setExpenses(responseData);
+      } catch (e) {
+        console.error("Error: ", e.message);
+      }
+    }
     fetchExpenses();
   }, []);
 
   return (
     <>
-        <ol>
         {expenses.map((expense, index) => {
             return (
             <>
-                <li key={index}>
-                <ul>
+                <ul key={index} >
                     <li>Description: {expense.description}</li>
                     <li>Amount: ${expense.amount}</li>
+                    <li>Category: {expense.category}</li>
                 </ul>
-                </li>
             </>
             );
         })}
-        </ol>
     </>
   );
 }
