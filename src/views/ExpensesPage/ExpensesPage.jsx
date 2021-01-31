8d2ea296100d46320a6ed64e9af7f7fb18a2f1af
derricklai33 <derricklai33@hotmail.com>
2021-01-31 15:53:22 +1100

    Renamed directory structure

--- src/views/ExpensesPage/ExpensesPage.jsx
+++ src/views/ExpensesPage/ExpensesPage.jsx
@@ -1,20 +1,21 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { NavLink } from "react-router-dom";
 import { Expenses } from "../../components/Expenses/Expenses"
+import { ExpensesAnalysis } from "../../components/Expenses/ExpensesAnalysisChart";
 
 import Button from "@material-ui/core/Button";
 import Icon from "@material-ui/core/Icon";
 import GridContainer from "components/Grid/GridContainer";
-import GridItem from "components/Grid/GridItem";
+// import GridItem from "components/Grid/GridItem";
 import Grid from "@material-ui/core/Grid";
 import Card from "components/Card/Card";
-import CardHeader from "components/Card/CardHeader";
-import CardIcon from "components/Card/CardIcon";
+// import CardHeader from "components/Card/CardHeader";
+// import CardIcon from "components/Card/CardIcon";
 import CardBody from "components/Card/CardBody";
-import CardFooter from "components/Card/CardFooter";
+// import CardFooter from "components/Card/CardFooter";
 import { makeStyles } from "@material-ui/core/styles";
 
-import { useAuth0 } from "@auth0/auth0-react";
+// import { useAuth0 } from "@auth0/auth0-react";
 
 import styles from "assets/jss/material-dashboard-react/views/expensesStyle";
 
@@ -28,21 +29,23 @@ export function ExpensesPage() {
       <div className={classes.newButton}>
       <NavLink to="/admin/expenses/new">
         <Button variant="contained" color="primary">
-          <Icon>add_circle</Icon> New Expense
+          <Icon >add_circle</Icon> New Expense
         </Button>
       </NavLink>
       </div>
 
-      <GridContainer className={classes.expensesContainer}>
+      <Grid container className={classes.expensesContainer}>
         <Grid item xs={12} md={12} lg={12} xl={8}>
-          <Grid className={classes.expenseAnalysisContainer}>
-            <Grid item xs={12} md={12} lg={7} xl={7}>
-              <Card></Card>
+          <Grid className={classes.expensesAnalysisContainer}>
+            <Grid item xs={12} md={12} lg={5} xl={5}>
+              <Card className={classes.expensesAnalysisCard}>
+                <ExpensesAnalysis/>
+              </Card>
             </Grid>
           </Grid>
           <Grid className={classes.expensesHistoryContainer}>
             <Grid item xs={12} md={12} lg={5} xl={5}>
-              <Card className={classes.expenseHistoryCard}>
+              <Card className={classes.expensesHistoryCard}>
                   <h3 className={classes.cardTitle}>Expenses History</h3>
                 <CardBody>
                   {/* Expenses component */}
@@ -52,7 +55,7 @@ export function ExpensesPage() {
             </Grid>
           </Grid>
         </Grid>
-      </GridContainer>
+      </Grid >
     </>
   );
 }
