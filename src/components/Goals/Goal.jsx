67821a000d3d6dfed75e400026670f5141aafad8
RoryMusinskas <devrorym@gmail.com>
2021-01-31 16:31:16 +1100

    Added a grid container and items, so the goal and the edit button are on one line.

--- src/components/Goals/Goal.jsx
+++ src/components/Goals/Goal.jsx
@@ -1,14 +1,16 @@
 /* -------- Import React core ------------ */
 import React from "react";
 /* -------- Import MaterialUI core ------------ */
-// import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core/styles";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
+import Edit from "@material-ui/icons/Edit";
+import Grid from "@material-ui/core/Grid";
 /* -------- Import Custom Components ---------- */
 import { Draggable } from "react-beautiful-dnd";
 
-// import styles from "assets/jss/material-dashboard-react/views/goalsPageStyle";
-// const useStyles = makeStyles(styles);
+import styles from "assets/jss/material-dashboard-react/views/goalsPageStyle";
+const useStyles = makeStyles(styles);
 
 // these are the styles for each goal when they are being dragged
 const getGoalStyle = (isDragging, draggableStyle) => ({
@@ -21,24 +23,36 @@ const getGoalStyle = (isDragging, draggableStyle) => ({
   }),
 });
 
+// handle click function for the edit icon
+const handleClick = () => {
+  console.log("clicked");
+};
+
 export default function Goal(props) {
-  // const classes = useStyles();
+  const classes = useStyles();
   return (
-    <Draggable draggableId={props.goal.id} index={props.index}>
-      {(provided, snapshot) => (
-        <ListItem
-          ref={provided.innerRef}
-          {...provided.draggableProps}
-          // this dragHandle is what controls the drag event. You could remove this from the container and add this to a component and make the draggable section a certain size etc.
-          {...provided.dragHandleProps}
-          style={getGoalStyle(
-            snapshot.isDragging,
-            provided.draggableProps.style
+    <Grid container>
+      <Grid item xs={10}>
+        <Draggable draggableId={props.goal.id} index={props.index}>
+          {(provided, snapshot) => (
+            <ListItem
+              ref={provided.innerRef}
+              {...provided.draggableProps}
+              // this dragHandle is what controls the drag event. You could remove this from the container and add this to a component and make the draggable section a certain size etc.
+              {...provided.dragHandleProps}
+              style={getGoalStyle(
+                snapshot.isDragging,
+                provided.draggableProps.style
+              )}
+            >
+              <ListItemText>{props.goal.content}</ListItemText>
+            </ListItem>
           )}
-        >
-          <ListItemText>{props.goal.content}</ListItemText>
-        </ListItem>
-      )}
-    </Draggable>
+        </Draggable>
+      </Grid>
+      <Grid item xs={2} className={classes.editGrid}>
+        <Edit onClick={handleClick} />
+      </Grid>
+    </Grid>
   );
 }
