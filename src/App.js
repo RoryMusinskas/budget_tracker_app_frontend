d3ea5ef7cc169896b8db6baef2324ef1e4620d6a
RoryMusinskas <devrorym@gmail.com>
2021-02-03 20:15:39 +1100

    Removed unneeded routes and RTL layout

--- src/App.js
+++ src/App.js
@@ -5,10 +5,7 @@ import { Route, Switch } from "react-router-dom";
 // Import Components/Views/Styles
 import "assets/css/material-dashboard-react.css?v=1.9.0";
 import Admin from "layouts/Admin";
-import RTL from "layouts/RTL";
 import LandingPage from "views/LandingPage/LandingPage";
-import { Profile } from "./components/UserProfile";
-import { Expenses } from "./components/Expenses";
 import Loading from "./components/Loading";
 
 // Import Auth0 code
@@ -29,9 +26,6 @@ export default function App() {
       <Switch>
         <Route exact path="/" component={LandingPage} />
         <ProtectedRoute path="/admin" component={Admin} />
-        <ProtectedRoute path="/rtl" component={RTL} />
-        <ProtectedRoute path="/profile" component={Profile} />
-        <ProtectedRoute path="/expenses" component={Expenses} />
       </Switch>
     </>
   );
