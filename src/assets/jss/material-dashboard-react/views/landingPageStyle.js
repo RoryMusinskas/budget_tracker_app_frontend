6aabe6d448a8a410ec3032b3c74689866c814108
RoryMusinskas <devrorym@gmail.com>
2021-02-04 18:16:25 +1100

    Added in the required styling for the landing page, removed old styles.

--- src/assets/jss/material-dashboard-react/views/landingPageStyle.js
+++ src/assets/jss/material-dashboard-react/views/landingPageStyle.js
@@ -8,14 +8,34 @@ import {
 const landingPageStyle = {
   landingPageContainer: {
     minHeight: "100vh",
+    display: "flex",
+    justifyContent: "center",
+    background: `linear-gradient(125deg, #ECFCFF 0%, #ECFCFF 40%, #B2FCFF calc(40% + 1px), #B2FCFF 60%, #5EDFFF calc(60% + 1px), #5EDFFF 72%, #3E64FF calc(72% + 1px), #3E64FF 100%)`,
+  },
+  loginContainer: {
+    display: "flex",
+    justifyContent: "flex-end",
+    height: "3vh",
+  },
+  buttons: {
+    padding: "20px 20px 0px 0px",
+    cursor: "pointer",
+    color: "black",
   },
   headerContainer: {
-    height: "70%",
+    display: "flex",
+    alignItems: "flex-end",
+    justifyContent: "center",
+  },
+  headerFooterContainer: {
     display: "flex",
     alignItems: "center",
     justifyContent: "center",
   },
-  header: {},
+  dropDownArrow: {
+    fontSize: "100px",
+    textDecoration: "none",
+  },
   buttonContainer: {
     height: "30%",
     display: "flex",
@@ -25,33 +45,22 @@ const landingPageStyle = {
   successText: {
     color: successColor[0],
   },
-  aboutPageContainer: {
+  featurePageContainer: {
     textAlign: "center",
     minHeight: "100vh",
     display: "flex",
-    alignItems: "center",
+    alignItems: "space-around",
     justifyContent: "center",
+    backgroundColor: "white",
   },
-  aboutHeaderContainer: {
-    height: "10vh",
-  },
-  aboutContainer: {
+  featureContainer: {
     display: "flex",
-    justifyContent: "space-around",
-    alignItems: "center",
+    justifyContent: "center",
+    flexWrap: "wrap",
+    marginBottom: "40px",
   },
-  aboutInfoContainer: {
-    minHeight: "30vh",
-    display: "flex",
-    justifyContent: "space-between",
-  },
-  aboutImageContainer: {
-    height: "60vh",
-  },
-  aboutImage: {
-    height: "100%",
-    objectFit: "cover",
-    width: "100%",
+  featureImage: {
+    height: "20vh",
   },
   featuresPageContainer: {
     minHeight: "100vh",
@@ -61,75 +70,13 @@ const landingPageStyle = {
     height: "10%",
   },
   featuresContainer: {
-    height: "90%",
     display: "flex",
     justifyContent: "space-around",
     alignItems: "center",
   },
-  featuresIncomeImage: {
-    height: "30%",
-    maxHeight: "350px",
-    objectFit: "cover",
-    width: "100%",
-  },
-  featuresExpenseImage: {
-    height: "30%",
-    maxHeight: "350px",
-    objectFit: "cover",
-    width: "100%",
-  },
-  featuresSharesImage: {
-    height: "30%",
-    maxHeight: "350px",
-    objectFit: "cover",
-    width: "100%",
-  },
   upArrowCardCategory: {
     width: "16px",
     height: "16px",
-  },
-  cardCategory: {
-    color: grayColor[0],
-    margin: "0",
-    fontSize: "14px",
-    marginTop: "0",
-    paddingTop: "10px",
-    marginBottom: "0",
-  },
-  cardCategoryWhite: {
-    color: "rgba(" + hexToRgb(whiteColor) + ",.62)",
-    margin: "0",
-    fontSize: "14px",
-    marginTop: "0",
-    marginBottom: "0",
-  },
-  cardTitle: {
-    color: grayColor[2],
-    marginTop: "0px",
-    minHeight: "auto",
-    fontWeight: "300",
-    fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
-    marginBottom: "3px",
-    textDecoration: "none",
-    "& small": {
-      color: grayColor[1],
-      fontWeight: "400",
-      lineHeight: "1",
-    },
-  },
-  cardTitleWhite: {
-    color: whiteColor,
-    marginTop: "0px",
-    minHeight: "auto",
-    fontWeight: "300",
-    fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
-    marginBottom: "3px",
-    textDecoration: "none",
-    "& small": {
-      color: grayColor[1],
-      fontWeight: "400",
-      lineHeight: "1",
-    },
   },
 };
 
