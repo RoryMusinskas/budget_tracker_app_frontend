59709062ef6c1dcee680f651d66b94f5886c7466
derricklai33 <derricklai33@hotmail.com>
2021-02-05 18:31:23 +1100

    Replaced New Expense button with Button component rendered from NewExpenseModal component and ran prettier

--- src/views/ExpensesPage/ExpensesPage.jsx
+++ src/views/ExpensesPage/ExpensesPage.jsx
@@ -1,9 +1,13 @@
+// React import
 import React, { useState } from "react";
+// react-router-dom import
 import { NavLink } from "react-router-dom";
-import { Expenses } from "../../components/Expenses/Expenses"
+// Expenses component import
+import { Expenses } from "../../components/Expenses/Expenses";
 import { ExpensesAnalysis } from "../../components/Expenses/ExpensesAnalysisChart";
 import { ExpensesAnalysisPie } from "../../components/Expenses/ExpensesAnalysisPie";
-
+import { NewExpenseModal } from "../../components/Expenses/NewExpenseModal"
+// Material-ui import
 import Button from "@material-ui/core/Button";
 import Icon from "@material-ui/core/Icon";
 import Grid from "@material-ui/core/Grid";
@@ -11,33 +15,30 @@ import Card from "components/Card/Card";
 import CardHeader from "components/Card/CardHeader";
 import CardBody from "components/Card/CardBody";
 import { makeStyles } from "@material-ui/core/styles";
-
-
+// Styled component import
 import styles from "assets/jss/material-dashboard-react/views/expensesStyle";
 
 const useStyles = makeStyles(styles);
 
 export function ExpensesPage() {
   const classes = useStyles();
+  // State hooks
   const [expenses, setExpenses] = useState([]);
-  const [ viewBarChart, setViewBarChart ] = useState(true);
+  const [viewBarChart, setViewBarChart] = useState(true);
 
+  // Sort button onclick to change between pie and bar chart
   function onClickToChangeChart() {
-    if(!viewBarChart) {
-      setViewBarChart(true)
+    if (!viewBarChart) {
+      setViewBarChart(true);
     } else {
-      setViewBarChart(false)
+      setViewBarChart(false);
     }
   }
 
   return (
     <>
       <div className={classes.newButton}>
-      <NavLink to="/admin/expenses/new">
-        <Button variant="contained" color="primary" className={classes.addExpenseButton}>
-          <Icon >add_circle</Icon> New Expense
-        </Button>
-      </NavLink>
+        <NewExpenseModal></NewExpenseModal>
       </div>
 
       <Grid container className={classes.expensesContainer}>
@@ -50,16 +51,23 @@ export function ExpensesPage() {
                 </CardHeader>
                 <CardBody>
                   {viewBarChart && <ExpensesAnalysis expenses={expenses} />}
-                  {!viewBarChart && <ExpensesAnalysisPie expenses={expenses}/>}
+                  {!viewBarChart && <ExpensesAnalysisPie expenses={expenses} />}
                   <div className="toggle-button">
-                    <Button variant="contained" color="primary" onClick={onClickToChangeChart} className={classes.sortButton}>Sort By</Button>
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      onClick={onClickToChangeChart}
+                      className={classes.sortButton}
+                    >
+                      Sort By
+                    </Button>
                   </div>
                 </CardBody>
               </Card>
             </Grid>
           </Grid>
         </Grid>
-        <Grid item xs={12} md={12} lg={4} xl={4} >
+        <Grid item xs={12} md={12} lg={4} xl={4}>
           <Grid className={classes.expensesHistoryContainer}>
             <Grid item xs={12} md={12} lg={12} xl={12}>
               <Card className={classes.expensesHistoryCard}>
@@ -67,13 +75,13 @@ export function ExpensesPage() {
                   <h3 className={classes.cardTitle}>Expenses History</h3>
                 </CardHeader>
                 <CardBody className={classes.expensesHistoryCardBody}>
-                  <Expenses expenses={expenses} setExpenses={setExpenses}/> 
+                  <Expenses expenses={expenses} setExpenses={setExpenses} />
                 </CardBody>
               </Card>
             </Grid>
           </Grid>
         </Grid>
-      </Grid >
+      </Grid>
     </>
   );
 }
