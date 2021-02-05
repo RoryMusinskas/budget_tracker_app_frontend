6817da46b2bf2252c48fd9cded44759d1320d493
RoryMusinskas <devrorym@gmail.com>
2021-02-05 13:02:12 +1100

    Removed the unneeded button import

--- src/components/Navbars/Navbar.jsx
+++ src/components/Navbars/Navbar.jsx
@@ -11,7 +11,6 @@ import Hidden from "@material-ui/core/Hidden";
 import Menu from "@material-ui/icons/Menu";
 // core components
 import AdminNavbarLinks from "./AdminNavbarLinks";
-import Button from "components/CustomButtons/Button";
 
 import styles from "assets/jss/material-dashboard-react/components/headerStyle";
 
