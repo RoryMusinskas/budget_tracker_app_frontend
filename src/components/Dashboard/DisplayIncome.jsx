b40b7c0e81e8d733fa2c5f1135049a4e80bcb302
derricklai33 <derricklai33@hotmail.com>
2021-02-07 01:38:10 +1100

    Updated component to return data that are created in the current year

--- src/components/Dashboard/DisplayIncome.jsx
+++ src/components/Dashboard/DisplayIncome.jsx
@@ -1,8 +1,10 @@
+// React import
 import React, { useState, useEffect } from 'react'
+// Auth0 import
 import { useAuth0 } from '@auth0/auth0-react'
 
 export function DisplayIncome(props){
-  const { setYearTotal } = props
+  const { setYearTotal, currentYear } = props
   const [income, setIncome] = useState([]);
   const { getAccessTokenSilently } = useAuth0();
 
@@ -19,25 +21,39 @@ export function DisplayIncome(props){
         }
       );
       const responseData = await response.json();
-      setIncome(responseData);
-      setTotal(responseData); 
+      setData(responseData)
     } catch (e) {
       console.error("Error: ", e.message);
     }
   }
 
+  // Mount component
   useEffect(() => {
     getUserIncomeData();
   }, [])
 
-  function setTotal(obj) {
+  // setData function returns array that corresponds to the current year
+  function setData(data) {
+    const tempArr = data.filter(item => {
+      if(item.date.split('-')[0] === `${currentYear}`) {
+        return data
+      }
+    })
+    setIncome(tempArr)
+    setTotal(tempArr)
+  }
+
+  // Sets total amount for this year lifts 
+  // state back to dashboard to display total
+  function setTotal(array) {
     let final = 0;
-    obj.forEach(item => {
+    array.forEach(item => {
       final += item.amount
     })
     setYearTotal(final)
   }
 
+  // Returns 15 of the most recent income from current year
   function incomesList() {
     if (income.length !== 0) {
       return (
