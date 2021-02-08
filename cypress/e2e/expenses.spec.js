e8723f5f73ce9218d96718869b15aac1060d13f4
derricklai33 <derricklai33@hotmail.com>
2021-02-08 22:44:57 +1100

    Cypress WIP

--- cypress/e2e/expenses.spec.js
+++ cypress/e2e/expenses.spec.js
@@ -1,15 +1,23 @@
-// describe("expenses page should be a protected route", () => {
-//   it("should not an allow a logged out user to the goals page", () => {
-//     cy.visit("/admin/expenses");
-//     cy.get(".c93b5c4a9").should("exist");
-//   })
-//   it("should allow the user to login with username and password", () => {
-//     cy.get("#username").type(Cypress.env("auth_username"));
-//     cy.get("#password").type(Cypress.env("auth_password"));
-//     cy.get(".cf251da15").click();
-//   })
-// })
+describe("expenses page should be a protected route", () => {
+  it("should not an allow a logged out user to the goals page", () => {
+    cy.visit("/admin/expenses");
+    cy.get(".c93b5c4a9").should("exist");
+  })
+  it("should allow the user to login with username and password", () => {
+    cy.get("#username").type(Cypress.env("auth_username"));
+    cy.get("#password").type(Cypress.env("auth_password"));
+    cy.get(".cf251da15").click();
+  })
+})
 
+describe("users are able to view expenses page after logging in", () => {
+  it("can view new expenses button", () => {
+    cy.findByText(/add expense/i)
+  })
+  it("can view analysis bar chart", () => {
+    cy.findByText(/expense analysis/i)
+  })
+})
 describe("expenses can be created after logging in", () => {
   it("user can create a new expense", () => {
     cy.visit("/admin/expenses");
